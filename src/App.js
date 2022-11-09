2e4b46e86e49f163d0c1ba168d3eb121a77e2905
Rajan Patel <88902043+rajanpatel126@users.noreply.github.com>
2022-11-09 12:02:08 +0530

    trying to add signup and login options but no thunderclient api had been used.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./components/Home";
 import About from "./components/About";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NoteState from "./context/notes/NoteState";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 function App() {
   return (
     <>
@@ -14,6 +16,8 @@ function App() {
             <Routes>
               <Route path="/about" element={<About />} />
               <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
             </Routes>
           </div>
         </BrowserRouter>
---
+++ src/components/Login.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Login = () => {
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+    }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3 my-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
